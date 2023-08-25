7610a72bd6db2f32769d598e13178b02e85f60e1
Luiz Hammerli <luizhammerli@gmail.com>
2023-08-25 09:10:16 -0300

    - Add presenter test file

---
+++ AppGardenTests/Features/Grid/Presenter/SearchImagePresenter.swift
@@ -0,0 +1,17 @@
+//
+//  SearchImagePresenter.swift
+//  AppGardenTests
+//
+//  Created by Luiz Diniz Hammerli on 25/08/23.
+//
+
+import XCTest
+@testable import AppGarden
+
+final class SearchImagePresenterTests: XCTestCase {
+    
+}
+
+private extension SearchImagePresenterTests {
+    
+}
