c11a771b47aa2a8a56e708204872ae21440de719
Luiz Hammerli <luizhammerli@gmail.com>
2023-09-16 21:25:11 -0300

    Update ImageDetailViewControllerSnapshotTests.swift

--- AppGardenTests/Features/Detail/View/ImageDetailViewControllerSnapshotTests.swift
+++ AppGardenTests/Features/Detail/View/ImageDetailViewControllerSnapshotTests.swift
@@ -12,7 +12,7 @@ import XCTest
 final class ImageDetailViewControllerSnapshotTests: XCTestCase {
     func test_snapshot_shouldShowDetailView() {
         let (_, navController) = makeSUT(tags: ["FirstTest", "SecondTest"])
-        assertSnapshot(matching: navController, as: .image(size: CGSize(width: 375, height: 667)))
+        assertSnapshot(matching: navController, as: .image(on: .iPhone8(.portrait)))
     }
     
     func test_snapshot_shouldShowDetailViewWithOneTag() {
